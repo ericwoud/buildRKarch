2c56d27cda07142824bd4b9158620bbb28175037
Eric Woudstra <ericwouds@gmail.com>
2023-03-03 17:32:40 +0100

    Update script as from BPI-R64

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+ 
 # Choose one of the following targets:
 # ----------------
 #TARGET="rk3288"
@@ -11,7 +11,7 @@ RKDEVICE="rock-5b"
 ATFDEVICE="sdmmc"
 ATFDEVICE="nvme"
 # ----------------
-
+ 
 # https://github.com/bradfa/flashbench.git, running multiple times:
 # sudo ./flashbench -a --count=64 --blocksize=1024 /dev/sda
 # Shows me that times increase at alignment of 8k
@@ -23,14 +23,14 @@ SD_BLOCK_SIZE_KB=8                   # in kilo bytes
 # bc -l <<<"$(cat /sys/block/mmcblk1/device/preferred_erase_size) /1024 /1024"
 # bc -l <<<"$(cat /sys/block/mmcblk1/queue/discard_granularity) /1024 /1024"
 SD_ERASE_SIZE_MB=4                   # in Mega bytes, align all partitions with this number
-
+ 
 MINIMAL_SIZE_UBOOT_MB=15             # Minimal size of uboot partition
 SPL_START_KB=32                      # Start of uboot partition
 MINIMAL_SIZE_BOOT_MB=150             # Minimal size of boot partition
 ROOT_END_MB=100%                     # Size of root partition
-#ROOT_END_MB=$(( 256*1024  ))        # Size 256GiB 
-
-
+#ROOT_END_MB=$(( 256*1024  ))        # Size 256GiB
+ 
+ 
 case $TARGET in
   rk3288)
     INTERFACENAME="end0"
@@ -55,38 +55,41 @@ case $TARGET in
     FSTABROOT="PARTLABEL=$PARTLABELROOT /     auto   defaults,noatime,nodiratime 0      0"
     # If the root file system is btrfs or XFS, the fsck order should be set to 0 instead of 1.
     ;;
-esac  
+esac 
 NEEDED_PACKAGES+=" base openssh wireless-regdb iproute2 f2fs-tools dtc mkinitcpio patch sudo"
 NEEDED_PACKAGES+=" linux-firmware networkmanager"
 EXTRA_PACKAGES="vim nano screen"
 SCRIPT_PACKAGES="wget ca-certificates udisks2 parted gzip bc f2fs-tools btrfs-progs dosfstools"
 SCRIPT_PACKAGES_ARCHLX="base-devel      uboot-tools  ncurses        openssl"
 SCRIPT_PACKAGES_DEBIAN="build-essential u-boot-tools libncurses-dev libssl-dev flex bison"
-
+ 
 LC="en_US.UTF-8"                     # Locale
 TIMEZONE="Europe/Paris"              # Timezone
 USERNAME="user"                      # User account name that is created
 USERPWD="admin"                      # User password
 ROOTPWD="admin"                      # Root password
-
-ALARM_MIRROR="http://de.mirror.archlinuxarm.org"
-
-QEMU_ARM="https://github.com/multiarch/qemu-user-static/releases/download/v5.2.0-11/x86_64_qemu-arm-static.tar.gz"
-QEMU_AARCH64="https://github.com/multiarch/qemu-user-static/releases/download/v5.2.0-11/x86_64_qemu-aarch64-static.tar.gz"
-
-ARCHBOOTSTRAP="https://raw.githubusercontent.com/tokland/arch-bootstrap/master/arch-bootstrap.sh"
-
+ 
+ALARM_MIRROR=http://de.mirror.archlinuxarm.org
+ 
+QEMU_ARM=https://github.com/multiarch/qemu-user-static/releases/download/v5.2.0-11/x86_64_qemu-arm-static.tar.gz
+QEMU_AARCH64=https://github.com/multiarch/qemu-user-static/releases/download/v5.2.0-11/x86_64_qemu-aarch64-static.tar.gz
+ 
+ARCHBOOTSTRAP=https://raw.githubusercontent.com/tokland/arch-bootstrap/master/arch-bootstrap.sh
+ 
 REPOKEY="DD73724DCA27796790D33E98798137154FE1474C"
 REPOURL='ftp://ftp.woudstra.mywire.org/repo/$arch'
 BACKUPREPOURL='https://github.com/ericwoud/buildRKarch/releases/download/repo-$arch'
-
+ 
 [ -f "./override.sh" ] && source ./override.sh
-
+ 
 export LC_ALL=C
 export LANG=C
 export LANGUAGE=C
-
+ 
 function finish {
+  trap 'echo got SIGINT' INT
+  trap 'echo got SIGEXIT' EXIT
+  [ -v prevautomountrules ] && $sudo rm -vf $noautomountrule
   if [ -v rootfsdir ] && [ ! -z $rootfsdir ]; then
     $sudo sync
     echo Running exit function to clean up...
@@ -104,68 +107,90 @@ function finish {
   unset rootfsdir
   [ -v sudoPID ] && kill -TERM $sudoPID
 }
-
+ 
 function waitdevlink {
   while [ ! -L "$1" ]; do
     echo "WAIT!"
     sleep 0.1
   done
 }
-
+ 
+function reinsert {
+  sync
+  bindpart=$(basename $(realpath /sys/block/$1/../..))
+  driver=$(realpath /sys/block/$1/device/driver)
+  echo -n $bindpart | $sudo tee $driver/unbind
+  echo -e "\nRe-inserting" $1
+  sleep 0.1
+  echo -n $bindpart | $sudo tee $driver/bind
+  echo
+  newdev=$(ls $driver/$bindpart/block | head -1)
+  echo "New Block Device: "$newdev
+  until lsblk /dev/$newdev >/dev/null 2>/dev/null; do sleep 0.1; done
+  $sudo partprobe "/dev/"$newdev
+  sync
+}
+ 
 function formatsd {
   echo ROOTDEV: $rootdev
-  realrootdev=$(lsblk -prno pkname $rootdev)
-  [ -z $realrootdev ] && exit
+  pkroot=$(lsblk -rno pkname $rootdev)
+  [ -z $pkroot ] && exit
   [ "$l" = true ] && skip="" || skip='\|^loop'
-  readarray -t options < <(lsblk --nodeps -no name,serial,size \
-                    | grep -v "^"${realrootdev/"/dev/"/}$skip \
-                    | grep -v 'boot0 \|boot1 \|boot2 ')
-  PS3="Choose device to format: "
-  select dev in "${options[@]}" "Quit" ; do
-    if (( REPLY > 0 && REPLY <= ${#options[@]} )) ; then
-      break
-    else exit
+  while true ; do
+    readarray -t options < <(lsblk --nodeps -no name,serial,size \
+                      | grep -v "^"${pkroot}$skip \
+                      | grep -v 'boot0 \|boot1 \|boot2 ')
+    PS3="Choose device to format: "
+    select dev in "${options[@]}" "Quit" ; do
+      if (( REPLY > 0 && REPLY <= ${#options[@]} )) ; then
+        break
+      else exit
+      fi
+   done
+    pkdev=${dev%% *}
+    device="/dev/"$pkdev
+    echo -n 'KERNELS=="'${pkdev}'", ENV{UDISKS_IGNORE}="1"' | $sudo tee $noautomountrule
+    for PART in `df -k | awk '{ print $1 }' | grep "${device}"` ; do $sudo umount $PART; done
+    $sudo parted -s "${device}" unit MiB print
+    echo -e "\nAre you sure you want to format "$device"???"
+    read -p "Type <format> to format: " prompt
+    [[ $prompt != "format" ]] && exit
+    minimalbootstart=$(( $SPL_START_KB + ($MINIMAL_SIZE_UBOOT_MB * 1024) ))
+    bootstart=0
+    while [[ $bootstart -lt $minimalbootstart ]]; do
+      bootstart=$(( $bootstart + ($SD_ERASE_SIZE_MB * 1024) ))
+    done
+    minimalrootstart=$(( $bootstart + ($MINIMAL_SIZE_BOOT_MB * 1024) ))
+    rootstart=0
+    while [[ $rootstart -lt $minimalrootstart ]]; do
+      rootstart=$(( $rootstart + ($SD_ERASE_SIZE_MB * 1024) ))
+    done
+    if [[ "$ROOT_END_MB" =~ "%" ]]; then
+      root_end_kb=$ROOT_END_MB
+    else
+      root_end_kb=$(( ($ROOT_END_MB/$SD_ERASE_SIZE_MB*$SD_ERASE_SIZE_MB)*1024))
+      echo $root_end_kb
     fi
+    $sudo wipefs --all --force "${device}"
+    $sudo dd of="${device}" if=/dev/zero bs=1024 count=$bootstart
+    $sudo sync
+    $sudo partprobe "${device}"
+    $sudo parted -s -- "${device}" mklabel gpt
+    [[ $? == 0 ]] && break # Succes creating GPT table
+    echo -e "\nFailed to format ${device} as GPT!\n"
+    reinsert $pkdev
   done
-  device="/dev/"${dev%% *}
-  for PART in `df -k | awk '{ print $1 }' | grep "${device}"` ; do $sudo umount $PART; done
-  $sudo parted -s "${device}" unit MiB print
-  echo -e "\nAre you sure you want to format "$device"???"
-  read -p "Type <format> to format: " prompt
-  [[ $prompt != "format" ]] && exit
-  minimalbootstart=$(( $SPL_START_KB + ($MINIMAL_SIZE_UBOOT_MB * 1024) ))
-  bootstart=0
-  while [[ $bootstart -lt $minimalbootstart ]]; do
-    bootstart=$(( $bootstart + ($SD_ERASE_SIZE_MB * 1024) ))
-  done
-  minimalrootstart=$(( $bootstart + ($MINIMAL_SIZE_BOOT_MB * 1024) ))
-  rootstart=0
-  while [[ $rootstart -lt $minimalrootstart ]]; do
-    rootstart=$(( $rootstart + ($SD_ERASE_SIZE_MB * 1024) ))
-  done
-  if [[ "$ROOT_END_MB" =~ "%" ]]; then
-    root_end_kb=$ROOT_END_MB
-  else
-    root_end_kb=$(( ($ROOT_END_MB/$SD_ERASE_SIZE_MB*$SD_ERASE_SIZE_MB)*1024))
-    echo $root_end_kb
-  fi
   case $TARGET in
     rk3288) formatsd_rk3288 $device ;;
     rk3588) formatsd_rk3588 $device ;;
-  esac  
+  esac 
   $sudo sync
   $sudo lsblk -o name,mountpoint,label,partlabel,size,uuid "${device}"
 }
-
+ 
 function formatsd_rk3288 {
   device=$1
-  $sudo wipefs --all --force "${device}"
-  $sudo sync
-  $sudo dd of="${device}" if=/dev/zero bs=1024 count=$bootstart
-  $sudo sync
-  $sudo partprobe "${device}"
   $sudo parted -s -- "${device}" unit kiB \
-    mklabel gpt \
     mkpart primary $bootstart $root_end_kb \
     mkpart primary $SPL_START_KB $bootstart \
     name 1 $PARTLABELROOT \
@@ -182,17 +207,11 @@ function formatsd_rk3288 {
   $sudo mkfs.ext4 -v -b $(( $blksize * 1024 ))  -L "${TARGET^^}-ROOT" \
     -E stride=$stride,stripe-width=$stripe "/dev/disk/by-partlabel/$PARTLABELROOT"
 }
-
+ 
 function formatsd_rk3588 {
   echo FORMAT OTHER
   device=$1
-  $sudo wipefs --all --force "${device}"
-  $sudo sync
-  $sudo dd of="${device}" if=/dev/zero bs=1024 count=$bootstart
-  $sudo sync
-  $sudo partprobe "${device}"
   $sudo parted -s -- "${device}" unit kiB \
-    mklabel gpt \
     mkpart primary $rootstart $root_end_kb \
     mkpart primary fat32 $bootstart $rootstart \
     mkpart primary $SPL_START_KB $bootstart \
@@ -208,7 +227,7 @@ function formatsd_rk3588 {
   $sudo mkfs.vfat -n "${TARGET^^}-BOOT" "/dev/disk/by-partlabel/$PARTLABELBOOT"
   $sudo mkfs.btrfs -f -L "${TARGET^^}-ROOT" "/dev/disk/by-partlabel/$PARTLABELROOT"
 }
-
+ 
 function bootstrap {
   if [ ! -d "$rootfsdir/etc" ]; then
     rm -f /tmp/downloads/$(basename $ARCHBOOTSTRAP)
@@ -218,7 +237,7 @@ function bootstrap {
     ls $rootfsdir
   fi
 }
-
+ 
 function rootfs {
   $sudo cp -rf --dereference -v ./rootfs/boot $rootfsdir
   echo "--- Following packages are installed:"
@@ -263,19 +282,19 @@ function rootfs {
   $schroot systemctl reenable NetworkManager
   find -L "rootfs/etc/systemd/system" -name "*.service"| while read service ; do
     $schroot systemctl reenable $(basename $service)
-  done
+ done
 }
-
+ 
 function chrootfs {
   echo "Entering chroot on image. Enter commands as if running on the target:"
   echo "Type <exit> to exit from the chroot environment."
   $schroot
 }
-
+ 
 function backuprootfs {
   $sudo tar -vcf "${BACKUPFILE}" -C $rootfsdir .
 }
-
+ 
 function restorerootfs {
   if [ -z "$(ls $rootfsdir)" ] || [ "$(ls $rootfsdir)" = "boot" ]; then
     $sudo tar -vxf "${BACKUPFILE}" -C $rootfsdir
@@ -285,7 +304,7 @@ function restorerootfs {
     echo "Root partition not empty!"
   fi
 }
-
+ 
 function installscript {
   if [ ! -f "/etc/arch-release" ]; then ### Ubuntu / Debian
     $sudo apt-get install --yes         $SCRIPT_PACKAGES $SCRIPT_PACKAGES_DEBIAN
@@ -308,6 +327,7 @@ function installscript {
   fi
   exit
 }
+ 
 function removescript {
   # On all linux's
   if [ $hostarch == "x86_64" ]; then # Script running on x86_64 so remove qemu
@@ -319,93 +339,98 @@ function removescript {
   fi
   exit
 }
-
+ 
+function ctrl_c() {
+  echo "** Trapped CTRL-C"
+  exit
+}
+ 
 [ $USER = "root" ] && sudo="" || sudo="sudo"
 [[ $# == 0 ]] && args="-c"|| args=$@
 cd "$(dirname -- "$(realpath -- "${BASH_SOURCE[0]}")")"
 while getopts ":rcpalbRSDAB" opt $args; do declare "${opt}=true" ; done
 echo OPTIONS: rootfs=$r postinst=$p chroot=$c apt=$a loopdev=$l
 trap finish EXIT
+trap ctrl_c INT
 shopt -s extglob
-
+ 
 echo -e "Target="$TARGET"\nTargetDevice="$RKDEVICE
 echo -e "Change target in first lines of script if necessairy..."
-
+ 
 if [ -n "$sudo" ]; then
   sudo -v
   ( while true; do sudo -v; sleep 40; done ) &
   sudoPID=$!
 fi
-
+ 
 if [[ "$(tr -d '\0' 2>/dev/null </proc/device-tree/compatible)" == *"$TARGET"* ]]; then
   echo "Running on $TARGET"
-  runontarget="true"
+  runningontarget="true"
 else
   echo "Not running on $TARGET"
-  runontarget="false"
+  runningontarget="false"
 fi
 hostarch=$(uname -m)
 echo "Host Arch:" $hostarch
-
+ 
 [ "$a" = true ] && installscript
 [ "$A" = true ] && removescript
-
-rootdev=$(lsblk -pilno name,type,mountpoint | grep -G 'part /$' |  head -n1 | cut -d " " -f1)
-
+set -m # send CTRL-C to children
+rootdev=$(lsblk -pilno name,type,mountpoint | grep -G 'part /$')
+rootdev=${rootdev%% *}
+noautomountrule="/run/udev/rules.d/10-no-automount.$$.rules"
 if [ "$S" = true ] && [ "$D" = true ]; then formatsd; exit; fi
-
-if [ -L "/dev/disk/by-partlabel/$PARTLABELROOT" ]; then
-  mountdev=$(realpath "/dev/disk/by-partlabel/$PARTLABELROOT")
-else
+mountdev="/dev/disk/by-partlabel/$PARTLABELROOT"
+if [ ! -L "$mountdev" ]; then
   echo "Not inserted! (Maybe not matching the target device on the card)"
   exit
 fi
-
-if [ "$rootdev" == "$mountdev" ];then
-  rootfsdir="" ; r="" ; R=""     # Protect root when running from it!
-  schroot=""
-else
-  rootfsdir="/mnt/rkrootfs"
-  schroot="$sudo unshare --mount --fork --kill-child --pid --root=$rootfsdir"
+if [ "$rootdev" == "$(realpath $mountdev)" ]; then
+  echo "Target device == Root device, exiting!"
+  exit
 fi
-
+pkdev=$(lsblk -no pkname ${mountdev})
+rootfsdir="/tmp/rkrootfs.$$"
+schroot="$sudo unshare --mount --fork --kill-child --pid --root=$rootfsdir"
+echo "SETUP="$SETUP
 echo "Rootfsdir="$rootfsdir
-echo "Mountdev="$mountdev
+echo "Mountdev="$(realpath $mountdev)
+echo -n 'KERNELS=="'${pkdev}'", ENV{UDISKS_IGNORE}="1"' | $sudo tee $noautomountrule
+echo
+$sudo umount $mountdev
 
-if [ ! -z $rootfsdir ]; then
-  $sudo umount $mountdev
-  [ -d $rootfsdir ] || $sudo mkdir $rootfsdir
-  [ "$b" = true ] && ro=",ro" || ro=""
-  $sudo mount --source $mountdev --target $rootfsdir \
-              -o exec,dev,noatime,nodiratime$ro
-  [[ $? != 0 ]] && exit
-  if [ "$R" = true ] ; then
-    read -p "Type <remove> to delete everything from the card: " prompt
-    [[ $prompt != "remove" ]] && exit
-    $sudo rm -rf $rootfsdir/{*,.*}
-    exit
-  fi
-  if [ -L "/dev/disk/by-partlabel/$PARTLABELBOOT" ]; then
-    $sudo mkdir -p $rootfsdir/boot
-    $sudo mount "/dev/disk/by-partlabel/$PARTLABELBOOT" $rootfsdir/boot
-  fi
-  if [ "$b" = true ] ; then backuprootfs; exit; fi
-  if [ "$B" = true ] ; then restorerootfs; exit; fi
-  [ "$r" = true ] && bootstrap
-  $sudo mount -t proc               /proc $rootfsdir/proc
-  [[ $? != 0 ]] && exit
-  $sudo mount --rbind --make-rslave /sys  $rootfsdir/sys
-  [[ $? != 0 ]] && exit
-  $sudo mount --rbind --make-rslave /dev  $rootfsdir/dev
-  [[ $? != 0 ]] && exit
-  $sudo mount --rbind --make-rslave /run  $rootfsdir/run
-  [[ $? != 0 ]] && exit
-  [ "$r" = true ] && rootfs
-  [ "$p" = true ] && $schroot rockchip-postinstall
-  [ "$c" = true ] && chrootfs
+[ -d $rootfsdir ] || $sudo mkdir $rootfsdir
+[ "$b" = true ] && ro=",ro" || ro=""
+
+$sudo mount --source $mountdev --target $rootfsdir \
+            -o exec,dev,noatime,nodiratime$ro
+[[ $? != 0 ]] && exit
+if [ -L "/dev/disk/by-partlabel/$PARTLABELBOOT" ]; then
+  $sudo mkdir -p $rootfsdir/boot
+  $sudo mount "/dev/disk/by-partlabel/$PARTLABELBOOT" $rootfsdir/boot
 fi
+ 
+if [ "$b" = true ] ; then backuprootfs; exit; fi
+if [ "$B" = true ] ; then restorerootfs; exit; fi
+if [ "$R" = true ] ; then
+  read -p "Type <remove> to delete everything from the card: " prompt
+  [[ $prompt != "remove" ]] && exit
+  $sudo rm -rf $rootfsdir/{*,.*}
+  exit
+fi
+[ "$r" = true ] && bootstrap
+$sudo mount -t proc               /proc $rootfsdir/proc
+[[ $? != 0 ]] && exit
+$sudo mount --rbind --make-rslave /sys  $rootfsdir/sys
+[[ $? != 0 ]] && exit
+$sudo mount --rbind --make-rslave /dev  $rootfsdir/dev
+[[ $? != 0 ]] && exit
+$sudo mount --rbind --make-rslave /run  $rootfsdir/run
+[[ $? != 0 ]] && exit
+[ "$r" = true ] && rootfs
+[ "$p" = true ] && $schroot rockchip-postinstall
+[ "$c" = true ] && chrootfs
 
-finish
 exit
 
 # sudo dd if=/dev/zero of=~/rock-5b-sdmmc.img bs=16M count=128 status=progress
